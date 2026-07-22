3a10c452875178b8a68c87ac8784bb92f557d3e3
静凇 <1477997018@qq.com>
2026-07-22 23:31:41 +0800

    commit

---
+++ src/main/java/com/bluesky/jetbrainslicenseserverhelp/controller/AliveController.java
@@ -0,0 +1,21 @@
+package com.bluesky.jetbrainslicenseserverhelp.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j(topic = "存活检查")
+@RestController
+@RequestMapping("/alive")
+public class AliveController {
+    @GetMapping()
+    public Map<String, String> alive() {
+        log.info("存活检查");
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "alive");
+        return response;
+    }
+}
