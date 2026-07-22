deb1fb661c2f3b9ce62a418a5cf8a6d5d73fb069
静凇 <1477997018@qq.com>
2026-07-23 04:13:43 +0800

    commit

--- src/main/java/com/bluesky/jetbrainslicenseserverhelp/controller/AliveController.java
+++ src/main/java/com/bluesky/jetbrainslicenseserverhelp/controller/AliveController.java
@@ -1,9 +1,9 @@
 package com.bluesky.jetbrainslicenseserverhelp.controller;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/alive")
 public class AliveController {
     @GetMapping()
-    public Map<String, Object> alive() {
+    public ResponseEntity<Void> alive() {
         String time = LocalDateTime.now().toString();
         log.info("检查时间: {}", time);
-        Map<String, Object> response = new HashMap<>();
-        response.put("status", "alive");
-        response.put("time", time);
-        return response;
+        return ResponseEntity.noContent()
+            .header("Time", time)
+            .build();
     }
 }
