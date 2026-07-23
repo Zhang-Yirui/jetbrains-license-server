cf277264fb376c6c33028bc8d7b432acbc9d1f27
静凇 <1477997018@qq.com>
2026-07-23 12:46:31 +0800

    commit

--- src/main/java/com/bluesky/jetbrainslicenseserverhelp/controller/AliveController.java
+++ src/main/java/com/bluesky/jetbrainslicenseserverhelp/controller/AliveController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j(topic = "存活检查")
@@ -13,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/alive")
 public class AliveController {
     @GetMapping()
-    public ResponseEntity<Void> alive() {
+    public ResponseEntity<Void> alive(
+            @RequestParam(value = "ts", required = false) String ts,
+            @RequestParam(value = "from", required = false) String from) {
         String time = LocalDateTime.now().toString();
-        log.info("检查时间: {}", time);
+        log.info("检查时间: {}, ts: {}, from: {}", time, ts, from);
         return ResponseEntity.noContent()
-            .header("Time", time)
-            .build();
+                .header("Time", time)
+                .build();
     }
 }
