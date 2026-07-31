75feb0e22b1413d02f8b865367c7018f26f7add6
静凇 <1477997018@qq.com>
2026-08-01 00:55:43 +0800

    commit

--- src/main/java/com/bd3qif/LicenseServer/controller/AliveController.java
+++ src/main/java/com/bd3qif/LicenseServer/controller/AliveController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,7 +22,6 @@ public class AliveController {
         @RequestParam(value = "ts", required = false, defaultValue = "1970-01-01T00:00:00") LocalDateTime ts,
         @RequestParam(value = "from", required = false, defaultValue = "unknown") String from) {
         log.info("alive 入参: skip_verify: {}, ts: {}, from: {}", verifyTime, ts, from);
-
         HttpStatusCode status = HttpStatus.NO_CONTENT;
         if (Boolean.TRUE.equals(verifyTime)) {
             LocalDateTime now = LocalDateTime.now();
@@ -31,8 +29,9 @@ public class AliveController {
             if (diff.abs().toMillis() > 5 * 1000) {
                 log.error("ts 超限: now:{}, ts:{}, diff: {}", now, ts, diff.toMillis());
                 status = HttpStatus.BAD_REQUEST;
+            } else {
+                log.info("now: {}, ts: {}, diff: {}, from: {}", now, ts, diff.toMillis(), from);
             }
-            log.info("now: {}, ts: {}, diff: {}, from: {}", now, ts, diff.toMillis(), from);
         }
         return ResponseEntity.status(status).header("Checker", from).build();
     }
